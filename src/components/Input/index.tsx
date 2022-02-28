e3fc2d31a30ae3621b342b58ed317e369ebc17e3
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-02-28 14:45:59 -0300

    feat: input styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef, useState } from 'react';
 
 import { useField } from '@unform/core';
 
@@ -10,29 +10,48 @@ interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Input({ name, label, ...rest }: IInputProps) {
-  const inputRef = useRef(null);
-  const { fieldName, defaultValue = '', registerField, error } = useField(name);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [hasError, setHasError] = useState(false);
+  const [hasFocus, setHasFocus] = useState(false);
+  const {
+    fieldName,
+    defaultValue = '',
+    registerField,
+    error,
+    clearError,
+  } = useField(name);
 
   useEffect(() => {
+    if (error) setHasError(error?.length > 0);
+    if (hasFocus) {
+      clearError();
+      setHasError(false);
+    }
     registerField({
       name: fieldName,
       ref: inputRef.current,
       path: 'value',
     });
-  }, [fieldName, registerField]);
+  }, [fieldName, registerField, error, hasFocus]);
+
   return (
     <>
       {label && <S.InputLabel htmlFor={fieldName}>{label}</S.InputLabel>}
 
-      <S.Input
-        ref={inputRef}
-        id={fieldName}
-        defaultValue={defaultValue}
-        autoComplete="off"
-        {...rest}
-      />
+      <S.InputWrapper>
+        <S.Input
+          ref={inputRef}
+          id={fieldName}
+          defaultValue={defaultValue}
+          autoComplete="off"
+          hasError={hasError}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+          {...rest}
+        />
+      </S.InputWrapper>
 
-      {error && <span style={{ color: '#f00' }}>{error}</span>}
+      {error && <S.ErrorMessage>{error}</S.ErrorMessage>}
     </>
   );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,23 +1,40 @@
+/* eslint-disable @typescript-eslint/indent */
 import styled from 'styled-components';
 
-export const Input = styled.input`
+export const Input = styled.input<{ hasError: boolean }>`
   width: 100%;
   margin-bottom: 15px;
   padding: 12px 16px;
+  outline: none;
+  border: 2px solid
+    ${({ theme, hasError }) => (hasError ? theme.colors.error : 'transparent')};
   border-radius: 4px;
-  border: 2px solid #ddd;
-  font-size: 15px;
-  color: #444;
-  transition: border-color 0.3s ease;
+  color: ${({ theme }) => theme.colors.white};
+  transition: border 0.3s ease;
+  background-color: ${({ theme }) => theme.colors.grey900};
 
   &:focus {
-    border-color: rgba(0, 0, 0, 0.2);
-    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
+    border-color: ${({ theme }) => theme.colors.purpleDark};
+  }
+
+  &:disabled {
+    opacity: 0.5;
   }
 `;
 
 export const InputLabel = styled.label`
+  color: ${({ theme }) => theme.colors.text};
   display: block;
   font-weight: bold;
   margin-bottom: 5px;
+`;
+
+export const ErrorMessage = styled.span`
+  color: ${({ theme }) => theme.colors.error};
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
