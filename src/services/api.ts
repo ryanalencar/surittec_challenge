fb3dbdb81c0f4e98a255902307747249cc2e3ef2
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-03-02 10:13:48 -0300

    feat: service api test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const dev = !!(process.env.NODE_ENV === 'development');
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: dev
+    ? 'http://localhost:3000/api'
+    : `${process.env.VIRTUAL_HOST}/api`,
 });
