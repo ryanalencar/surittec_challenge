6753353723f36dd667519ffcaa4e5bdfa27e748c
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-03-02 10:20:28 -0300

    refactor: api prod url

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,5 +5,5 @@ const dev = !!(process.env.NODE_ENV === 'development');
 export const api = axios.create({
   baseURL: dev
     ? 'http://localhost:3000/api'
-    : `${process.env.VIRTUAL_HOST}/api`,
+    : 'https://surittec-challenge.vercel.app/api',
 });
